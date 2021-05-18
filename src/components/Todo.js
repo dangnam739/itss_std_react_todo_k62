2c6f8e6edc8501ec8364a276450461e106406cf3
EC2 Default User <ec2-user@ip-172-31-50-16.ec2.internal>
2021-05-18 04:34:55 +0000

    Save Todo on Firestore

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,46 +1,19 @@
 import React, { useState } from 'react';
 
-/* 
-  【Todoのデータ構成】
-　・key：Todoを特定するID（String）
-　・text：Todoの内容（String）
-　・done：完了状態（Boolean true:完了済み,, false:未完了）
-*/
-
 /* コンポーネント */
 import TodoItem from './TodoItem';
 import Input from './Input';
 import Filter from './Filter';
 
 /* カスタムフック */
-import useStorage from '../hooks/storage';
+import useFbStorage from '../hooks/fbStorage';
 
 /* ライブラリ */
 import {getKey} from "../lib/util";
 
 function Todo() {
-  // const [items, putItems] = React.useState([
-  //     /* テストコード 開始 */
-  //   { key: getKey(), text: '日本語の宿題', done: false },
-  //   { key: getKey(), text: 'reactを勉強する', done: false },
-  //   { key: getKey(), text: '明日の準備をする', done: false },
-  //   /* テストコード 終了 */
-  // ]);
-  const [items, putItems, clearItems] = useStorage();
-  
-  const handleCheck = checked => {
-    const newItems = items.map(item => {
-      if (item.key === checked.key) {
-        item.done = !item.done;
-      }
-      return item;
-    });
-    putItems(newItems);
-  };
-  
-  const handleAdd = text => {
-    putItems([...items, {key: getKey(), text, done:false}]);
-  };
+  // const [items, putItems, clearItems] = useStorage();
+  const [items, addItem, updateItem, clearItems] = useFbStorage();
   
   const [filter, setFilter] = React.useState('ALL');
 
@@ -49,12 +22,34 @@ function Todo() {
     if (filter === 'TODO') return !item.done;
     if (filter === 'DONE') return item.done;
   });
+  
+  const handleCheck = checked => {
+    // const newItems = items.map(item => {
+    //   if (item.key === checked.key) {
+    //     item.done = !item.done;
+    //   }
+    //   return item;
+    // });
+    // putItems(newItems);
+    updateItem(checked);
+  };
+  
+  const handleAdd = text => {
+    // putItems([...items, { key: getKey(), text, done: false }]);
+    addItem({ text, done: false });
+  };
+  
   const handleFilterChange = value => setFilter(value);
 
   return (
-    <div className="panel">
+    <article class="panel is-danger">
       <div className="panel-heading">
-        ITSS ToDoアプリ
+        <span class="icon-text">
+          <span class="icon">
+            <i class="fas fa-calendar-check"></i>
+          </span>
+          <span> ITSS Todoアプリ</span>
+        </span>
       </div>
       <Input onAdd={handleAdd} />
       <Filter
@@ -62,8 +57,8 @@ function Todo() {
         value={filter}
       />
       {displayItems.map(item => (
-        <TodoItem
-          key={item.key}
+        <TodoItem 
+          key={item.id}
           item={item}
           onCheck={handleCheck}
         />
@@ -76,10 +71,8 @@ function Todo() {
           全てのToDoを削除
         </button>
       </div>
-    </div>
+    </article>
   );
 }
-
-
 
 export default Todo;
